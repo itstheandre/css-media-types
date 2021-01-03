83267b9554a47e11739c72be0a30af0791379642
Andre <itstheandre@gmail.com>
2021-01-03 18:10:03 +0100

    fix(main): change main function documentation and to trigger release

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { ICSSMediaTypesOptions } from "./interface";
 import { allMediaQueries, prevVersions } from "./utils";
 
+/**
+ * In case you wish to also get previous (now deprecated) media query types
+ * Look for more information in the [specification page](https://drafts.csswg.org/mediaqueries/#media-types).
+ * If this is not the easiest read, you can also check [MDN](https://developer.mozilla.org/pt-BR/docs/Web/CSS/@media)
+ */
 export const cssMediaTypes = (options?: ICSSMediaTypesOptions) => {
   const opts = options ? options : {};
   const { prev = false } = opts as ICSSMediaTypesOptions;
